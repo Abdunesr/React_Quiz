522617cbcb49d68c8c9a72d5c2b00bbb7ee2add7
Abdunesr <nesredinabdelah33@gmail.com>
2024-11-29 21:56:58 +0300

    Creating the server and Fetching the questions logic to console for further usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Mains from "./Mains";
 import Header from "./Header";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error "));
+  });
   return (
     <div className="app">
       <Header />
